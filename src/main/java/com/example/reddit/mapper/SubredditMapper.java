28b9dc31350995ef236d82c08564eb8b9ce98ad4
Adnan Farooque <adnan.dipta89@gmail.com>
2021-07-02 13:15:04 +0600

    Subreddit Mapping Complete

--- src/main/java/com/example/reddit/mapper/SubredditMapper.java
+++ src/main/java/com/example/reddit/mapper/SubredditMapper.java
@@ -1,4 +1,23 @@
 package com.example.reddit.mapper;
 
-public class SubredditMapper {
+import com.example.reddit.dto.SubredditDto;
+import com.example.reddit.model.Subreddit;
+import com.example.reddit.model.Post;
+//import jdk.internal.loader.*;
+import org.mapstruct.*;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+    @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPosts(List<Post> numberOfPosts) {
+        return numberOfPosts.size();
+    }
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
 }
--- src/main/java/com/example/reddit/service/SubredditService.java
+++ src/main/java/com/example/reddit/service/SubredditService.java
@@ -1,8 +1,11 @@
 package com.example.reddit.service;
 
 import com.example.reddit.dto.*;
+import com.example.reddit.exceptions.*;
+import com.example.reddit.mapper.*;
 import com.example.reddit.model.*;
 import com.example.reddit.repository.*;
+//import jdk.internal.loader.*;
 import lombok.*;
 import lombok.extern.slf4j.*;
 import org.springframework.stereotype.*;
@@ -18,10 +21,11 @@ import static java.util.stream.Collectors.toList;
 public class SubredditService {
 
     private final SubredditRepository subredditRepository;
+    private final SubredditMapper subredditMapper;
 
     @Transactional
     public SubredditDto save(SubredditDto subredditDto) {
-        Subreddit save = subredditRepository.save(mapSubredditDto(subredditDto));
+        Subreddit save = subredditRepository.save(subredditMapper.mapDtoToSubreddit(subredditDto));
         subredditDto.setId(save.getId());
         return subredditDto;
     }
@@ -30,20 +34,26 @@ public class SubredditService {
     public List<SubredditDto> getAll() {
         return subredditRepository.findAll()
                 .stream()
-                .map(this::mapToDto)
+                .map(subredditMapper::mapSubredditToDto)
                 .collect(toList());
     }
 
-    private SubredditDto mapToDto(Subreddit subreddit) {
-        return SubredditDto.builder().name(subreddit.getName())
-                .id(subreddit.getId())
-                .numberOfPosts(subreddit.getPosts().size())
-                .build();
+    public SubredditDto getSubreddit(Long id) {
+        Subreddit subreddit = subredditRepository.findById(id)
+                .orElseThrow(() -> new SpringRedditException("Not subreddit found with the id - " + id));
+        return subredditMapper.mapSubredditToDto(subreddit);
     }
 
-    private Subreddit mapSubredditDto(SubredditDto subredditDto) {
-        return Subreddit.builder().name(subredditDto.getName())
-                .description(subredditDto.getDescription())
-                .build();
-    }
+//    private SubredditDto mapToDto(Subreddit subreddit) {
+//        return SubredditDto.builder().name(subreddit.getName())
+//                .id(subreddit.getId())
+//                .numberOfPosts(subreddit.getPosts().size())
+//                .build();
+//    }
+//
+//    private Subreddit mapSubredditDto(SubredditDto subredditDto) {
+//        return Subreddit.builder().name(subredditDto.getName())
+//                .description(subredditDto.getDescription())
+//                .build();
+//    }
 }
